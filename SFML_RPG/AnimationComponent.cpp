3d6b4b516d37e377e963876561816c99e208f355
KeltorHD <keltorplaylife@gmail.com>
2019-12-10 18:07:18 +0300

    Создание компонента для анимации ч1

--- SFML_RPG/AnimationComponent.cpp
+++ SFML_RPG/AnimationComponent.cpp
@@ -1,9 +1,18 @@
 #include "AnimationComponent.h"
 
-AnimationComponent::AnimationComponent()
+AnimationComponent::AnimationComponent(sf::Sprite& sprite, sf::Texture& texture_sheet)
+	: sprite(sprite), textureSheet(texture_sheet)
 {
 }
 
 AnimationComponent::~AnimationComponent()
 {
+}
+
+//func
+
+
+void AnimationComponent::update(const float& dt)
+{
+
 }
--- SFML_RPG/AnimationComponent.h
+++ SFML_RPG/AnimationComponent.h
@@ -18,21 +18,74 @@ private:
 	class Animation
 	{
 	public:
-		Animation();
+		//var
+		sf::Sprite& sprite;
+		sf::Texture& textureSheet;
+		float animationTimer;
+		float timer;
+		int width;
+		int height;
+
+		sf::IntRect startRect;
+		sf::IntRect currentRect;
+		sf::IntRect endRect;
+
+		Animation(sf::Sprite& sprite, 
+			sf::Texture& texture_sheet, float animation_timer, 
+			int start_x, int start_y, 
+			int end_x, int end_y,
+			int width, int height)
+			: sprite(sprite), textureSheet(texture_sheet), animationTimer(animation_timer), width(width), height(height)
+		{
+			this->startRect = sf::IntRect(start_x, start_y, width, height);
+			this->currentRect = this->startRect;
+			this->endRect = sf::IntRect(end_x, end_y, width, height);
+
+			this->sprite.setTexture(this->textureSheet, true);
+			this->sprite.setTextureRect(this->startRect);
+		}
 		
 		//func
-		void play();
+		void update(const float& dt)
+		{
+			//update timer
+			this->timer = 10.f * dt;
+			if (this->timer >= this->animationTimer)
+			{
+				//reset timer
+				this->timer = 0.f;
+
+				//Animate
+				if (this->currentRect != this->endRect)
+				{
+					this->currentRect.left += this->width;
+				}
+				else //reset
+				{
+					this->currentRect.left = this->startRect.left;
+				}
+			}
+		}
 		void pause();
 		void reset();
 	};
-
+	sf::Sprite& sprite;
+	sf::Texture& textureSheet;
 	std::map<std::string, Animation> animations;
 public:
 
-	AnimationComponent();
+	AnimationComponent(sf::Sprite& sprite, sf::Texture& texture_sheet);
 	virtual ~AnimationComponent();
 
 	//func
+	void addAnimation(const std::string key);
+
+	void startAnimation(const std::string animation);
+	void pauseAnimation(const std::string animation);
+	void resetAnimation(const std::string animation);
+
+	void update(const float& dt);
+
 };
 
 
