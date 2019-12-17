b25e84b79e0ddfe4f9dde01d8632a950d2f8703b
KeltorHD <keltorplaylife@gmail.com>
2019-12-17 18:17:24 +0300

    Работа с картой плиток ч1

--- SFML_RPG/Tile.h
+++ SFML_RPG/Tile.h
@@ -1,6 +1,15 @@
 #ifndef TILE_H
 #define TILE_H
 
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+#include "SFML\System.hpp"
+#include "SFML\Window.hpp"
+#include "SFML\Graphics.hpp"
 
 class Tile
 {
